0281e81f36dc159f8657bc6854c3cc92700bd1f8
Viva Dev <109100020+okpalavivian@users.noreply.github.com>
2022-12-06 22:54:43 +0000

    add

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,16 +1,52 @@
 import React from "react";
+import "./Footer.scss"
 const Footer = () => {
-  return <div className="footer">
+  return (<div className="footer">
   <div className="top">
 
 <div className="item">
 <h1>Categories</h1>
+<span>Women</span>
+<span>Men</span>
+<span>Shoes</span>
+<span>Accessories</span>
+<span>New Arrivals</span>
 </div>
-    <div className="item"></div>
-    <div className="item"></div>
-    <div className="item"></div>
+    <div className="item">
+      <h1>
+        links
+      </h1>
+      <span>FAQ</span>
+      <span>Pages</span>
+      <span>Stores</span>
+      <span>Compare</span>
+      <span>Cookies</span>
+     
+    </div>
+    <div className="item">
+   <h1>About</h1>
+   <span>Lorem ipsum dolor sit amet, 
+    consectetur adipiscing elit, sed do eiusmod 
+    tempor incididunt ut labore et dolore magna
+     aliqua. Ut enim ad minim veniam, quis nostrud
+      exercitation ullamco laboris nisi ut aliquip
+       ex ea commodo consequat</span>
+       </div>
+       <div className="item">
+   <h1>Contact</h1>
+   <span>Lorem ipsum dolor sit amet, 
+    consectetur adipiscing elit, sed do eiusmod 
+    tempor incididunt ut labore et dolore magna
+     aliqua. Ut enim ad minim veniam, quis nostrud
+      exercitation ullamco laboris nisi ut aliquip
+       ex ea commodo consequat</span>
+       
+    </div>
   </div>
-    <div className="bottom"></div>
-  </div>;
+    <div className="bottom">
+      <div ClassName="left"></div>
+      <div ClassName="right"></div>
+    </div>
+  </div>)
 };
 export default Footer;
