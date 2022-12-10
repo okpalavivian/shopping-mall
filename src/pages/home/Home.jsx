5d83b25a48b7d08a2004b4129605caee8870d803
Viva Dev <109100020+okpalavivian@users.noreply.github.com>
2022-12-10 16:43:50 +0000

    Add

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import Slider from "../../components/Slider/Slider";
 import FeaturedProduct from "../../components/FeaturedProduct/FeaturedProduct";
 import "./Home.scss"
+import Categories from "../../components/Categories/Categories";
 
 const Home = () => {
   return( <div className="home">
     <Slider />
     <FeaturedProduct type="featured" />
+    <Categories />
     <FeaturedProduct type="trending" />
   </div>)
 };
