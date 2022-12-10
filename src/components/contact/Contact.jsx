bf486699c90bb4dd8b27c0deae943f6af5a4815d
Viva Dev <109100020+okpalavivian@users.noreply.github.com>
2022-12-10 17:00:42 +0000

    Add

---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./Contact.scss";
+
+
+const Contact = () => {
+  return (
+    <div className='contact'>Contact
+      <div className="wrapper">
+
+<span></span>
+
+      </div>
+    </div>
+  )
+}
+
+export default Contact
