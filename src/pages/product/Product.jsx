80ad9fa3f34a2dd6d30892582b4d70d8c18c75cb
Viva Dev <109100020+okpalavivian@users.noreply.github.com>
2023-01-03 16:42:37 +0000

    update

--- src/pages/product/Product.jsx
+++ src/pages/product/Product.jsx
@@ -1,22 +1,43 @@
 import React from "react";
 import "./Product.scss"
-
-const images =[
- " https://postimg.cc/0z7JCL5s",
- "https://i.postimg.cc/zvwY1pvG/DSC-3990.jpg"
-];
+import { useState } from "react";
 
 const Product = () => {
-  return <div className="product">
+
+  const [selectedImg, setSelectedImg] = useState(0);
+  
+  const images =[
+   "https://i.postimg.cc/nhgtfbx2/DSC-3915.jpg",
+   "https://i.postimg.cc/zvwY1pvG/DSC-3990.jpg"
+  ];
+
+  return (
+  <div className="product">
 <div className="left">
-  <img src={images [0]} alt="" onClick={e=>setSelectedImg(0)}/>
+<div className="images">
+<img src={images [0]} alt="" onClick={e=>setSelectedImg(0)}/>
   <img src={images [1]} alt="" onClick={e=>setSelectedImg(1)}/>
 </div>
 <div className="mainImg">
-  <img src={images[setSelectedImg]} alt="" />
+  <img src={images [selectedImg]} alt="" />
 </div>
-<div className="right"></div>
+</div>
+<div className="right">
+  <h1>Title</h1>
+  <span>$18.8</span>
+  <p>
+  Lorem ipsum dolor sit amet, 
+  consectetur adipiscing elit, sed do eiusmod tempor
+   incididunt ut labore et dolore magna aliqua.
+    Ut enim ad minim veniam, quis nostrud exercitation 
+    ullamco laboris nisi ut aliquip ex ea commodo consequat
+    </p>
+    <div className="quantity">
+      <button onClick={()=>setQuantity(prev=>prev-1)}>-</button>
+      <button onClick={()=>setQuantity(prev=>prev-1)}>+</button>
+    </div>
+</div>
 
-  </div>;
+  </div>)
 };
 export default Product;
